81123943fbfe459d9bed9399eaa50ab96a50bc2d
Rawser Spicer <rwspicer@alaska.edu>
2022-08-11 14:40:24 -0800

    added views

--- waterdata/__init__.py
+++ waterdata/__init__.py
@@ -1,5 +1,8 @@
 __version__ = '0.1.0'
 
 from . import web_services
-from . import formats
+from . import formats, views
 # from .web_services import *
+
+# NOTE THis for future reference
+# https://help.waterdata.usgs.gov/codes-and-parameters/codes#TS
--- waterdata/formats.py
+++ waterdata/formats.py
@@ -3,13 +3,14 @@
 """
 from pandas import DataFrame, read_table
 from io import StringIO
+import yaml
 
-def convert_to_DataFrame(text):
+def rdb_to_DataFrame(text):
     table = read_table(StringIO(text), comment='#')
     table = table[1:].reset_index(drop=True)
     return table
 
-def parse_header(text):
+def rdb_to_header(text):
     header_lines = [ l for l in text.split('\n') if len(l) >0 and l[0] == '#']
     return '\n'.join(header_lines)
 
@@ -24,11 +25,22 @@ def save_as_csv(filename, callback, **kwargs):
     kwargs['format'] = 'rdb'
     response = callback(**kwargs)
     
-    header =  parse_header(response)
-    table = convert_to_DataFrame(response)
+    header =  rdb_to_header(response)
+    table = rdb_to_DataFrame(response)
     with open(filename, 'w') as fd:
         fd.write(header)
         fd.write('\n')
     table.to_csv(filename, mode='a', index=0)
 
 
+def to_yaml(callback, **kwargs):
+
+    ## define fomrats supported
+    kwargs['format'] = kwargs['format'] if 'format' in kwargs else "dict"
+    data = callback(**kwargs)
+    return yaml.dump(data, sort_keys=False)
+
+def save_as_yaml(filename, callback, **kwargs):
+
+    with open(filename, 'w') as fd:
+        fd.write(to_yaml(callback, **kwargs))
